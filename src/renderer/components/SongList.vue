37248141d1f8f8c22f46130752303183506f73c9
PNNutkung <pipatpol.t@ku.th>
2018-01-22 13:58:42 +0700

    Change CI setting

---
+++ src/renderer/components/SongList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="dropzone" @dragover="onDragover" @drop="onDrop">
+    <ul>
+      <li v-for="(song, index) in songs" :key="index">
+        {{ song.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+  data () {
+    return {
+      songs: []
+    }
+  },
+  methods: {
+    onDrop: function (e) {
+      e.stopPropagation()
+      e.preventDefault()
+      console.log(e.dataTransfer.files)
+      _.values(e.dataTransfer.files).forEach(song => {
+        this.songs.push(song)
+      })
+    },
+    onDragover: function (e) {
+      e.preventDefault()
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .dropzone {
+    border: 5px dashed skyblue;
+    height: 95vh;
+  }
+</style>
