149fd8688acfb0dc894215baa4e9b85ec8a72788
Pakpon <pakpon.j@ku.th>
2018-04-17 16:08:40 +0700

    Test if localstorage is available

--- src/renderer/components/MusicPlayer.vue
+++ src/renderer/components/MusicPlayer.vue
@@ -86,6 +86,13 @@ export default {
           })
         })
     })
+    if (this.storageAvailable('localStorage')) {
+      // Yippee! We can use localStorage awesomeness
+      console.log(`LocalStorage is available.`)
+    } else {
+      // Too bad, no localStorage for us
+      console.log(`LocalStorage isn't available.`)
+    }
   },
   beforeDestroy () {
     this.stopSong()
@@ -113,6 +120,29 @@ export default {
       await this.stopSong()
       await this.playSong(songInfo, this.updateTime)
       this.updateTime()
+    },
+    storageAvailable: function (type) {
+      let storage
+      try {
+        storage = window[type]
+        let x = '__storage_test__'
+        storage.setItem(x, x)
+        storage.removeItem(x)
+        return true
+      } catch (e) {
+        return e instanceof DOMException && (
+          // everything except Firefox
+          e.code === 22 ||
+          // Firefox
+          e.code === 1014 ||
+          // test name field too, because code might not be present
+          // everything except Firefox
+          e.name === 'QuotaExceededError' ||
+          // Firefox
+          e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+          // acknowledge QuotaExceededError only if there's something already stored
+          storage.length !== 0
+      }
     },
     ...mapActions([
       'setSongs',
